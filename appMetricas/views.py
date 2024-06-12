734882b4dbdaadb55f0b9c923a86d1ee8e51e1c2
Alexiotovv <gpsalexvasquez@gmail.com>
2024-06-11 19:51:49 -0500

    otf_change

--- appMetricas/views.py
+++ appMetricas/views.py
@@ -104,8 +104,8 @@ def generar_imagenes_cobranzas(df):
     
     if platform.system() == 'Windows':
         # Rutas para Windows
-        font_path = "C:/Windows/Fonts/Arial"
-        font_path_numero = "C:/Windows/Fonts/Arial"
+        font_path = "C:/Windows/Fonts/Arial.otf"
+        font_path_numero = "C:/Windows/Fonts/Arial.otf"
     else:
         # Rutas para Linux
         font_path = "/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf"
